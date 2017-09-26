81f58a0797a94e0b358e024e89c5860123748846
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-09-26 11:51:52 +0800

    Insert a button click event "private void btnOK_Click(object sender, EventArgs e)" inside the class StationNameDialog, its purpose is to pass station name and MDI parent window handle to the child window., meanwhile open the TestPanel window.

---
+++ AUPS/StationNameDialog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Amphenol.AUPS
+{
+    public partial class StationNameDialog : Form
+    {
+        private MainWindow parent;
+
+        public MainWindow Parent
+        {
+            get
+            {
+                return parent;
+            }
+            set
+            {
+                parent = value;
+            }
+        }
+
+        public StationNameDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (textBoxStationName.Text.Length == 0)
+                return;
+            TestPanel child = new TestPanel(parent, textBoxStationName.Text);
+            Close();
+            child.Show();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
