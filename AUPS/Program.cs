19451bed8614170bb4f08a2ba7e0d9a5f641178a
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-09-26 11:10:46 +0800

    1. Change the namespace to Amphenol.AUPS,  comply with the naming rule : CompanyName.ProjectName
    2. Add the file comments in the head of every code file.

--- AUPS/Program.cs
+++ AUPS/Program.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿/***************************************************************************************************
+ * Project      : AUPS
+ * Description  : Amphenol Universal Platform and Sequenzer, develop an universal test platform MDI
+ *              : application, served for Amphenol Automatical Production Testing System.
+ * =================================================================================================
+ * File name    : Program.cs
+ * Description  : This file implements the Main() method inside the class Amphenol.AUPS.Program, it
+ *              : is the entry point of the entire AUPS application.
+ * Creator      : Frederick Hsu
+ * Creation date: Mon.  25 Sep., 2017
+ * Copyright(C) 2017    Shanghai Amphenol Airwave Communication Electronics Co., Ltd.
+ * All rights reserved.
+ * 
+ ***************************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AUPS
+namespace Amphenol.AUPS
 {
     static class Program
     {
@@ -16,7 +31,7 @@ namespace AUPS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
