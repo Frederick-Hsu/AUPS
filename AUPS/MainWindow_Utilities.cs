d7e3743acfdc5e31acce6935f6a1af9ed04b8dce
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-09-27 10:12:33 +0800

    为MainWindow类新增一个部分类，用于存放用户的一些Utilities功能。

---
+++ AUPS/MainWindow_Utilities.cs
@@ -0,0 +1,18 @@
+﻿/***************************************************************************************************
+ * File name    : MainWindow_Utilities.cs
+ * Description  : This code file will provide some utlities of class MainWindow.
+ * Creator      : Frederick Hsu
+ * Creation date: Tue.  26 Sep. 2017
+ * Copyright(C) 2017    Shanghai Amphenol Airwave Communication Electronics Co., Ltd.
+ * All rights reserved.
+ * 
+ ***************************************************************************************************/
+
+using System.Windows.Forms;
+
+namespace Amphenol.AUPS
+{
+    partial class MainWindow
+    {
+    }
+}
