86d8d7cf950e6d87a73d7386ae87af4c02d8340e
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-09-28 15:04:45 +0800

    修改StationNameDialog的界面设计。

--- AUPS/StationNameDialog.Designer.cs
+++ AUPS/StationNameDialog.Designer.cs
@@ -32,6 +32,8 @@
             this.textBoxStationName = new System.Windows.Forms.TextBox();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.labelStationNumber = new System.Windows.Forms.Label();
+            this.textBoxStationNumber = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // labelStationName
@@ -41,11 +43,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.labelStationName.AutoSize = true;
             this.labelStationName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.labelStationName.Location = new System.Drawing.Point(38, 54);
+            this.labelStationName.Location = new System.Drawing.Point(28, 55);
+            this.labelStationName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelStationName.Name = "labelStationName";
-            this.labelStationName.Size = new System.Drawing.Size(121, 31);
+            this.labelStationName.Size = new System.Drawing.Size(163, 26);
             this.labelStationName.TabIndex = 0;
-            this.labelStationName.Text = "Station : ";
+            this.labelStationName.Text = "Station Name : ";
             // 
             // textBoxStationName
             // 
@@ -53,19 +56,21 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxStationName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBoxStationName.Location = new System.Drawing.Point(165, 54);
+            this.textBoxStationName.Location = new System.Drawing.Point(211, 55);
+            this.textBoxStationName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxStationName.Name = "textBoxStationName";
-            this.textBoxStationName.Size = new System.Drawing.Size(438, 38);
-            this.textBoxStationName.TabIndex = 1;
+            this.textBoxStationName.Size = new System.Drawing.Size(243, 32);
+            this.textBoxStationName.TabIndex = 2;
             // 
             // btnOK
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnOK.Location = new System.Drawing.Point(426, 122);
+            this.btnOK.Location = new System.Drawing.Point(320, 110);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(119, 43);
-            this.btnOK.TabIndex = 2;
+            this.btnOK.Size = new System.Drawing.Size(89, 35);
+            this.btnOK.TabIndex = 3;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
@@ -74,25 +79,55 @@
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnCancel.Location = new System.Drawing.Point(106, 122);
+            this.btnCancel.Location = new System.Drawing.Point(80, 110);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(119, 43);
+            this.btnCancel.Size = new System.Drawing.Size(89, 35);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // labelStationNumber
+            // 
+            this.labelStationNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelStationNumber.AutoSize = true;
+            this.labelStationNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelStationNumber.Location = new System.Drawing.Point(28, 18);
+            this.labelStationNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelStationNumber.Name = "labelStationNumber";
+            this.labelStationNumber.Size = new System.Drawing.Size(132, 26);
+            this.labelStationNumber.TabIndex = 4;
+            this.labelStationNumber.Text = "Station No. :";
+            // 
+            // textBoxStationNumber
+            // 
+            this.textBoxStationNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxStationNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxStationNumber.Location = new System.Drawing.Point(211, 15);
+            this.textBoxStationNumber.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxStationNumber.Name = "textBoxStationNumber";
+            this.textBoxStationNumber.Size = new System.Drawing.Size(243, 32);
+            this.textBoxStationNumber.TabIndex = 1;
+            // 
             // StationNameDialog
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(663, 197);
+            this.ClientSize = new System.Drawing.Size(497, 160);
+            this.Controls.Add(this.textBoxStationNumber);
+            this.Controls.Add(this.labelStationNumber);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.textBoxStationName);
             this.Controls.Add(this.labelStationName);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "StationNameDialog";
@@ -109,5 +144,7 @@
         private System.Windows.Forms.TextBox textBoxStationName;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label labelStationNumber;
+        private System.Windows.Forms.TextBox textBoxStationNumber;
     }
 }
