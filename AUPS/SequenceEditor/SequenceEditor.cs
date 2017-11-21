1c536833087112750a010b034a3685eaaf6da62f
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-11-21 11:07:02 +0800

    Implement the AfterSelect event method for the TreeView control : private void treeViewSequence_AfterSelect(object sender, TreeViewEventArgs e)
    and the SelectedIndexChanged event method for ComboBox control : private void comboBoxLimitType_SelectedIndexChanged(object sender, EventArgs e)

--- AUPS/SequenceEditor/SequenceEditor.cs
+++ AUPS/SequenceEditor/SequenceEditor.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Amphenol.Seq;
+
 namespace Amphenol.AUPS
 {
     public partial class SequenceEditor : Form
@@ -21,6 +23,36 @@ namespace Amphenol.AUPS
         {
             InitializeComponent();
             MdiParent = parent;
+        }
+
+        private void treeViewSequence_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (treeViewSequence.SelectedNode.Tag is Step)
+            {
+                DisplayStepContentOnPanel((Step)treeViewSequence.SelectedNode.Tag);
+            }
+        }
+
+        private void comboBoxLimitType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridViewTestSpec.Columns.Clear();
+
+            if (comboBoxLimitType.Text == "Numerical")
+            {
+                dataGridViewTestSpec.Columns.Add("LowerLimit", "Lower");
+                dataGridViewTestSpec.Columns.Add("TypicalLimit", "Typical");
+                dataGridViewTestSpec.Columns.Add("UpperLimit", "Upper");
+                dataGridViewTestSpec.Columns.Add("Result", "Result");
+
+                dataGridViewTestSpec.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            else if (comboBoxLimitType.Text == "String")
+            {
+                dataGridViewTestSpec.Columns.Add("ExpectLimit", "Expect");
+                dataGridViewTestSpec.Columns.Add("Result", "Result");
+
+                dataGridViewTestSpec.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
         }
     }
 }
