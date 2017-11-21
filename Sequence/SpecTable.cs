ee741fa94d3ae3610a91178b370a12a5c772238f
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-11-21 10:49:03 +0800

    In the Sequence.csproj project, add a new class SpecTable, but this class SpecTable is not implemented, perhaps will not be used later.

---
+++ Sequence/SpecTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Amphenol.Seq
+{
+    public class SpecTable
+    {
+    }
+}
