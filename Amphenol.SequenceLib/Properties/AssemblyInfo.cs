0badb520f56970b6adc8c92ad17c631cad3624e3
Frederick Hsu <frederick.xu@amphenolmcp.com>
2017-12-22 16:10:39 +0800

    Upgrade Amphenol.SequenceLib version to V0.1.0.0.   This is the 1st release.

--- Amphenol.SequenceLib/Properties/AssemblyInfo.cs
+++ Amphenol.SequenceLib/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Amphenol.SequenceLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Sequence class library, served for AUPS project.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Shanghai Amphenol Airwave Communication Electronics Co., Ltd.")]
 [assembly: AssemblyProduct("Amphenol.SequenceLib")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Shanghai Amphenol Airwave 2017")]
+[assembly: AssemblyTrademark("SAA(R)")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
