fd0457aafb2277c5770d6e2e2d36f177cffaa4ce
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-01-24 11:36:32 +0800

    Implement the test item function as an example, test engineer only needs to change the function name "OpenCom" and implement the function body as your actual test project required.
            private static bool OpenCom(List<string> stepParameters,
                                        out string stepResult,
                                        out string stepStatus,
                                        out string stepErrorCode,
                                        out string stepErrorDesc)

---
+++ Amphenol.Project.X577/MyTestItems.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+namespace Amphenol.Project.X577
+{
+    partial class TestItems
+    {
+        private static bool OpenCom(List<string> stepParameters,
+                                    out string stepResult,
+                                    out string stepStatus,
+                                    out string stepErrorCode,
+                                    out string stepErrorDesc)
+        {
+            string comnum = stepParameters[0];
+            int baud = Convert.ToInt32(stepParameters[1]);
+            int databits = Convert.ToInt32(stepParameters[2]);
+
+            SerialPort serial;
+            try
+            {
+                serial = new SerialPort(comnum, baud, Parity.None, databits, StopBits.One);
+                serial.Open();
+            }
+            catch (System.IO.IOException exception)
+            {
+                stepResult = "NG";
+                stepStatus = "Fail";
+                stepErrorCode = "COMEXCE";
+                stepErrorDesc = exception.Message;
+                return false;
+            }
+
+            stepResult = "OK";
+            stepStatus = "Pass";
+            stepErrorCode = "";
+            stepErrorDesc = "";
+            return true;
+        }
+    }
+}
