38bf4e2343c6bfed88016d7ab31423cc07aeb041
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-01-25 14:45:19 +0800

    1. 使用多线程来操作正常的测试步骤执行和进度条显示，计时测试所消耗的时间。
    2. 使用delegate来实时追踪测试已消耗的时间，和进度条的进度显示。

--- AUPS/TestPanel/TestPanel_Home.cs
+++ AUPS/TestPanel/TestPanel_Home.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using Amphenol.SequenceLib;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Amphenol.AUPS
 {
     partial class TestPanel
     {
+        private static bool exitFlag = false;
+        private static DateTime timer;
+        private static int finishedStepNum = 0;
+
         private void btnStart_Click(object sender, EventArgs e)
         {
             bool flag = false;
+            exitFlag = false;
 
             if (testSeq == null)
                 return;
+
+            /* Launch a thread to record the testing time specifically  */
+            Thread timerThread = new Thread(TraceTestingTime);
+            timerThread.IsBackground = true;    /* And this thread must run in background */
+            timerThread.Start(int.Parse(testSeq.TotalStepNum().ToString()));
+
+            timer = DateTime.Now;       /* Start to record the testing time */
+            finishedStepNum = 0;        /* Reset this finishedStepNum when each test cycle starts */
 
             /* Indicating the "Testing..." status */
             labelIndicator.Text = "Testing......";
@@ -24,6 +38,7 @@ namespace Amphenol.AUPS
                 foreach (TestStep step in block.TestStepList)
                 {
                     flag = step.PerformTestStep(testSeq.SeqXmlDoc);      /* Perform current test step */
+                    finishedStepNum++;
 
                     string[] stepSubItems = new string[3];
                     stepSubItems[0] = step.StepNum;
@@ -42,6 +57,7 @@ namespace Amphenol.AUPS
                         listViewTestItems.Items[index - 1].ForeColor = System.Drawing.Color.Red;
 
                         SaveTestRecord();
+                        exitFlag = true;
                         return;
                     }
                     else
@@ -54,6 +70,8 @@ namespace Amphenol.AUPS
             labelIndicator.Text = "PASS";
             labelIndicator.ForeColor = System.Drawing.Color.Green;
             SaveTestRecord();
+            exitFlag = true;
+            return;
         }
 
         private void SaveTestRecord()
@@ -65,6 +83,29 @@ namespace Amphenol.AUPS
             string recordFileName = currDir + "\\" + sn + "_" + timestamp + ".xml";
 
             testSeq.SaveAsTestRecord(recordFileName);
+        }
+
+        private delegate void TraceTestingTimeDelegate(object number);
+
+        private void TraceTestingTime(object number)
+        {
+            if (progressBarTestProgress.InvokeRequired)
+            {
+                TraceTestingTimeDelegate delg = TraceTestingTime;
+                progressBarTestProgress.Invoke(delg, number);
+            }
+            else
+            {
+                progressBarTestProgress.Maximum = (int)number;
+                while (exitFlag == false)
+                {
+                    progressBarTestProgress.Value = finishedStepNum;
+                    toolStripStatusLabelElapsedTime.Text = "Elapsed time : " + DateTime.Now.Subtract(timer).ToString();
+
+                    Application.DoEvents();     /* Allow other task to win enough CPU time slices to run. */
+                }
+                toolStripStatusLabelElapsedTime.Text = "Elapsed time : " + DateTime.Now.Subtract(timer).ToString();
+            }
         }
     }
 }
