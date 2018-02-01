8afb74769509c67a17a414fe28850f043e5b66f5
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-02-01 11:43:54 +0800

    Setup the IP address of TCP Client computer.

--- TCP_Client/Program.cs
+++ TCP_Client/Program.cs
@@ -14,7 +14,7 @@ namespace TCP_Client
 
         static void Main(string[] args)
         {
-            IPAddress ip = IPAddress.Loopback;
+            IPAddress ip = IPAddress.Parse("192.168.30.52");
             Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             try
