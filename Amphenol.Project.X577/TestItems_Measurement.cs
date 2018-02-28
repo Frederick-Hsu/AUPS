5c353b4217e1f4223c23e06ad931fce8f6762fcc
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-02-28 10:52:45 +0800

    For example project X577, implement 3 test item functions : InitializeDigitalMultimeter(...),   CloseDMM(...) and MeasureResistorOver2Wires(...)

--- Amphenol.Project.X577/TestItems_Measurement.cs
+++ Amphenol.Project.X577/TestItems_Measurement.cs
@@ -15,27 +15,79 @@ namespace Amphenol.Project.X577
                                                         out string stepErrorCode,
                                                         out string stepErrorDesc)
         {
-            bool successFlag = false;
-            string ipAddr = stepParameters[0];
-            int portNum = Convert.ToInt32(stepParameters[1]);
+            int successFlag;
+            string dmmVisaAddress = stepParameters[0];
 
-            dmm = new DigitalMultiMeter_34461A(ipAddr, portNum);
-            successFlag = dmm.PingDMM();
-            if (successFlag == true)
+            dmm = new DigitalMultiMeter_34461A();
+            successFlag = dmm.Open(dmmVisaAddress);
+            successFlag = dmm.GetInstrumentIdentifier(out stepResult);
+
+            if (successFlag == 0)
             {
-                stepResult = "OK";
                 stepStatus = "Pass";
                 stepErrorCode = "";
                 stepErrorDesc = "";
             }
             else
             {
+                stepStatus = "Fail";
+                stepErrorCode = "DMM01";
+                stepErrorDesc = "Fail to initialize the DMM.";
+            }
+            return (successFlag == 0);
+        }
+
+        private static bool CloseDMM(out string stepResult,
+                                     out string stepStatus,
+                                     out string stepErrorCode,
+                                     out string stepErrorDesc)
+        {
+            int successFlag = dmm.Close();
+            if (successFlag == 0)
+            {
+                stepResult = "OK";
+                stepStatus = "Pass";
+                stepErrorCode = string.Empty;
+                stepErrorDesc = string.Empty;
+            }
+            else
+            {
                 stepResult = "NG";
                 stepStatus = "Fail";
-                stepErrorCode = "PINGE";
-                stepErrorDesc = "Fail to initalize and ping the DMM over ethernet.";
+                stepErrorCode = "DMMCL";
+                stepErrorDesc = "Fail to close the DMM.";
             }
-            return successFlag;
+            return (successFlag == 0);
+        }
+
+        private static bool MeasureResistorOver2Wires(List<string> limits,
+                                                      out string stepResult,
+                                                      out string stepStatus,
+                                                      out string stepErrorCode,
+                                                      out string stepErrorDesc)
+        {
+            float resistor;
+            float lowerLimit = Convert.ToSingle(limits[0]),
+                  expectedLimit = Convert.ToSingle(limits[1]),
+                  upperLimit = Convert.ToSingle(limits[2]);
+
+            int successFlag = dmm.MeasureResistorVia2Wires(out resistor);
+            stepResult = resistor.ToString();
+
+            if ((resistor > lowerLimit) && (resistor < upperLimit))
+            {
+                stepStatus = "Pass";
+                stepErrorCode = "";
+                stepErrorDesc = "";
+                return true;
+            }
+            else
+            {
+                stepStatus = "Fail";
+                stepErrorCode = "RES01";
+                stepErrorDesc = "Resistance value is out of the range.";
+                return false;
+            }
         }
     }
 }
