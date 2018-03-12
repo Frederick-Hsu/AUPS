99ef0cfd61317d6673b82c9df7badb3fb7cf0691
Frédérique Hsu (徐贊) <frederique.hsu@gmail.com>
2018-03-12 10:32:10 +0800

    Write the test items to verify how to access the Keysight DC power supply E3646A.

--- Amphenol.Project.X577/TestItems_InstrumentsAccessing.cs
+++ Amphenol.Project.X577/TestItems_InstrumentsAccessing.cs
@@ -58,4 +58,60 @@ namespace Amphenol.Project.X577
             return (successFlag == 0);
         }
     }
+
+    partial class TestItems
+    {
+        private static Amphenol.Instruments.Keysight.DCPowerSupply_E3646A powerSupply;
+
+        private static bool InitializeDCPowerSupply(List<string> stepParameters,
+                                                    out string stepResult,
+                                                    out string stepStatus,
+                                                    out string stepErrorCode,
+                                                    out string stepErrorDesc)
+        {
+            string visaAddress = stepParameters[0];
+
+            powerSupply = new Instruments.Keysight.DCPowerSupply_E3646A();
+            int successFlag = powerSupply.Open(visaAddress);
+
+            successFlag = powerSupply.GetInstrumentIdentifier(out stepResult);
+
+            if (successFlag == 0)
+            {
+                stepStatus = "Pass";
+                stepErrorCode = "";
+                stepErrorDesc = "";
+            }
+            else
+            {
+                stepStatus = "Fail";
+                stepErrorCode = "PWRSU01";
+                stepErrorDesc = "Fail to initialize the Keysight DC power supply E3646A.";
+            }
+            return (successFlag == 0);
+        }
+
+        private static bool CloseDCPowerSupply(out string stepResult,
+                                               out string stepStatus,
+                                               out string stepErrorCode,
+                                               out string stepErrorDesc)
+        {
+            int successFlag = powerSupply.Close();
+            if (successFlag == 0)
+            {
+                stepResult = "OK";
+                stepStatus = "Pass";
+                stepErrorCode = "";
+                stepErrorDesc = "";
+            }
+            else
+            {
+                stepResult = "NOK";
+                stepStatus = "Fail";
+                stepErrorCode = "PWRSU02";
+                stepErrorDesc = "Fail to close this DC power supply.";
+            }
+            return (successFlag == 0);
+        }
+    }
 }                     
