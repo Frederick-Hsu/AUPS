e90797a75dfb9aaa6b3a9035942a6f0a38930078
Frederick Hsu(徐贊) <frederick.xu@amphenolmcp.com>
2018-04-02 17:14:40 +0800

    实现Keysight network analyzer的Trigger system部分的命令集。

--- Amphenol.Instruments/Keysight/NetworkAnalyzer_E5071C.cs
+++ Amphenol.Instruments/Keysight/NetworkAnalyzer_E5071C.cs
@@ -661,5 +661,186 @@ namespace Amphenol.Instruments.Keysight
             errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
             return QueryErrorStatus(out response);
         }
+
+        /* :TRIG:SOUR BUS */
+        public int SelectTriggerSource(string triggerSource /* 4 types : INTernal, EXTernal, MANual, BUS */ )
+        {
+            int errorno, count;
+            string command = ":TRIGger:SOURce " + triggerSource, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* TRIG */
+        public int TriggerMeasurement()
+        {
+            int errorno, count;
+            string command = ":TRIGger:IMMediate", response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :TRIG:SING */
+        public int SingleTriggerMeasurement()
+        {
+            int errorno, count;
+            string command = ":TRIGger:SINGle", response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :INIT1 */
+        public int SwitchToStartupStateForChannel(int channelNum)
+        {
+            int errorno, count;
+            string command = ":INITiate" + channelNum + ":IMMediate", response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :INIT1:CONT OFF */
+        public int TurnOnOffContinuousInitiationModeForChannel(int channelNum = 1, string on_off = "ON")
+        {
+            int errorno, count;
+            string command = ":INITiate" + channelNum + ":CONTinuous " + on_off, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :TRIG:SCOP ACT */
+        public int SelectActiveChannelToTrigger(string activeChannel = "ALL" /* only 2 kinds : "ALL" or "ACT" */)
+        {
+            int errorno, count;
+            string command = ":TRIGger:SEQuence:SCOPe " + activeChannel, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* *SRE 128 */
+        public int SetServiceRequestEnableRegister(byte registerValue = 0x80 /* range : [0x00, 0xFF] */)
+        {
+            int errorno, count;
+            string command = "*SRE " + registerValue, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :STAT:OPER:ENAB 16 */
+        public int SetOperationStatusEnableRegister(ushort registerValue = 0x0010 /* range : [0x00, 0xFFFF] */ )
+        {
+            int errorno, count;
+            string command = ":STATus:OPERation:ENABle " + registerValue, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :STAT:OPE:ENAB? */
+        public int QueryOperationRegisterStatus(out ushort registerValue)
+        {
+            int errorno, count;
+            string command = ":STATus:OPERation:ENABle?", response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            errorno = visa32.viRead(analyzerSession, out response, 256);
+            registerValue = Convert.ToUInt16(response);
+            return errorno;
+        }
+
+        /* :STAT:OPER:PTR 16 */
+        public int SetPositiveTransitionFilterOfOperationStatusRegister(ushort positiveTransitionFilterValue = 0x4030 /* range : [0, 0xFFFF] */)
+        {
+            int errorno, count;
+            string command = ":STATus:OPERation:PTRansition " + positiveTransitionFilterValue, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* STAT:OPER:PTR? */
+        public int QueryPositiveTransitionFilterOperationStatus(out ushort operationStatusValue)
+        {
+            int errorno, count;
+            string command = ":STATus:OPERation:PTRansition?", response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            errorno = visa32.viRead(analyzerSession, out response, 256);
+            operationStatusValue = Convert.ToUInt16(response);
+            return errorno;
+        }
+
+        /* :STAT:OPER:NTR 16 */
+        public int SetNegativeTransitionFilterOfOperationStatusRegister(ushort negativeTransitionFilterValue = 0x0000)
+        {
+            int errorno, count;
+            string command = ":STATus:OPERation:NTRansition " + negativeTransitionFilterValue, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :STAT:OPER:NTR? */
+        public int QueryNegativeTransitionFilterOperationStatus(out ushort operationStatusValue)
+        {
+            int errorno, count;
+            string command = ":STATus:OPERation:NTRansition?", response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            errorno = visa32.viRead(analyzerSession, out response, 256);
+            operationStatusValue = Convert.ToUInt16(response);
+            return errorno;
+        }
+
+        /* *CLS */
+        public int ClearInstrument()
+        {
+            int errorno, count;
+            string command = "*CLS",     /* Clean up all of the instrument, including the entire error queue */
+                   response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);      /* Make sure all errors had been cleaned up. */
+        }
+
+        /* *OPC? */
+        public int QueryEventsCompletionStatus(out int status)
+        {
+            int errorno, count;
+            string command = "*OPC?", response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            errorno = visa32.viRead(analyzerSession, out response, 256);
+            status = Convert.ToInt32(response);
+            return errorno;
+        }
+
+        /* :TRIG:POIN ON */
+        public int TurnOnOffPointTrigger(string on_off = "OFF" /* Only "ON" and "OFF" can be chosen. */)
+        {
+            int errorno, count;
+            string command = ":TRIGger:SEQuence:POINt " + on_off.ToUpper(), response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :TRIG:EXT:LLAT ON */
+        public int ToggleOnOffLowLatencyExternalTrigger(string on_off = "OFF")
+        {
+            int errorno, count;
+            string command = ":TRIGger:SEQuence:EXTernal:LLATency:STATe " + on_off.ToUpper(), response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :TRIG:EXT:DEL 0.05 */
+        public int SetExternalTriggerDelayTime(double delayTime = 0.00 /* range : [0, 1]second, unit : second, resolution : 10us */)
+        {
+            int errorno, count;
+            string command = ":TRIGger:SEQuence:EXTernal:DELay " + delayTime, response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :TRIG:AVER ON */
+        public int ToggleOnOffAveragingTrigger(string on_off = "OFF")
+        {
+            int errorno, count;
+            string command = ":TRIGger:SEQuence:AVERage " + on_off.ToUpper(), response;
+            errorno = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
     }
 }
+                                    
