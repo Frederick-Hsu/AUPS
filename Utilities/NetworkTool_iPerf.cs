1e3ab2afb5ff1390cec27f5fe555f30478e5db07
Frederick Hsu(徐贊) <frederick.xu@amphenolmcp.com>
2018-05-21 14:15:37 +0800

    打算用事件编程机制来编写Network_iPerf类，拟解决当前iPerf命令行进程运行时间过长而导致图形界面被冻结僵死的问题，但可惜没有成功实现出来。
    留待以后来研究吧。

--- Utilities/NetworkTool_iPerf.cs
+++ Utilities/NetworkTool_iPerf.cs
@@ -15,11 +15,12 @@ namespace Utilities
             iPerfCmd = iPerfExeFilePath;
         }
 
-        public void StartIperf(bool createNoWindow = true,          /* Option : whether not to create DOS window */
+#if true
+        public void StartIperf(bool createNoWindow = false,         /* Option : whether not to create DOS window */
                                bool useShellExecute = false,        /* Option : whether to start the Shell process */
                                bool redirectStandardInput = true,   /* Option : whether to redirect the input stream */
                                bool redirectStandardOutput = true,  /* Option : whether to redirect the output stream */
-                               bool redirectStandardError = true    /* Option : whether to redirect the error stream */)
+                               bool redirectStandardError = true)   /* Option : whether to redirect the error stream */
         {
             iPerfCmdProc = new System.Diagnostics.Process();
             iPerfCmdProc.StartInfo.FileName = "cmd.exe";
@@ -48,5 +49,52 @@ namespace Utilities
             executionReturnedResult = iPerfCmdProc.StandardOutput.ReadToEnd();
             iPerfCmdProc.WaitForExit();
         }
+#else
+        System.Diagnostics.ProcessStartInfo procInfo;
+        public void StartIperf(bool createNoWindow = false,
+                               bool useShellExecute = false,
+                               bool redirectStandardInput = true,
+                               bool redirectStandardOutput = true,
+                               bool redirectStandardError = true)
+        {
+            procInfo = new System.Diagnostics.ProcessStartInfo("cmd.exe");
+            procInfo.CreateNoWindow = createNoWindow;
+            procInfo.UseShellExecute = useShellExecute;
+            procInfo.RedirectStandardInput = redirectStandardInput;
+            procInfo.RedirectStandardOutput = redirectStandardOutput;
+            procInfo.RedirectStandardError = redirectStandardError;
+
+            iPerfCmdProc = System.Diagnostics.Process.Start(procInfo);
+            iPerfCmdProc.OutputDataReceived += IPerfCmdProc_OutputDataReceived;
+            iPerfCmdProc.ErrorDataReceived += IPerfCmdProc_ErrorDataReceived;
+        }
+
+        private void IPerfCmdProc_ErrorDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IPerfCmdProc_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteIperf(string iPerfOptions, out string executionReturnedResult)
+        {
+            string commandStr = iPerfCmd + " " + iPerfOptions + "\r";
+            iPerfCmdProc.StandardInput.WriteLine(commandStr + "&exit");
+            iPerfCmdProc.StandardInput.AutoFlush = true;
+
+            iPerfCmdProc.BeginOutputReadLine();
+            iPerfCmdProc.BeginErrorReadLine();
+
+            iPerfCmdProc.Exited += IPerfCmdProc_Exited;
+        }
+
+        private void IPerfCmdProc_Exited(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+#endif
     }
 }
