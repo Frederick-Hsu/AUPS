e8285f71f5240c570990cb5a1ded055068927a51
Frederick Hsu(徐贊) <frederick.xu@amphenolmcp.com>
2018-05-21 14:10:44 +0800

    1. 移除对AsyncExecuteDOSProcess/项目目录的修改追踪。
    2  添加一个新代码文件TestItems.cs，拟用来实现天线测试的具体项。

---
+++ AUPS/Tools/TestItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AUPS.Tools
+{
+    partial class GMVehicleAntennaTesting
+    {
+        #region Antenna physical layer test items
+        #endregion
+
+        /******************************************************************************************/
+
+        #region Antenna application layer test items
+        #endregion
+    }
+}
