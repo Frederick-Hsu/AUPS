6047d49bda776284e653e57f89f5fbe28a2ec02c
Frederick Hsu(徐贊) <frederick.xu@amphenolmcp.com>
2018-06-12 18:35:59 +0800

    Define the class "SignalAnalyzer_N9020A", implement its constructor, Open() and Close() methods.

---
+++ Amphenol.Instruments/Keysight/SignalAnalyzer_N9020A.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Amphenol.Instruments.Keysight
+{
+    public class SignalAnalyzer_N9020A
+    {
+        private int resourceMrg;
+        private int session;
+
+        public SignalAnalyzer_N9020A()
+        {
+            resourceMrg = 0;
+            session = 0;
+        }
+
+        public int Open(string visaAddress)
+        {
+            int viError = visa32.viOpenDefaultRM(out resourceMrg);
+            if (viError != visa32.VI_SUCCESS)
+            {
+                return viError;
+            }
+            viError = visa32.viOpen(resourceMrg, visaAddress, visa32.VI_NO_LOCK, visa32.VI_TMO_IMMEDIATE, out session);
+            if (viError != visa32.VI_SUCCESS)
+                return viError;
+            return viError;
+        }
+
+        public int Close()
+        {
+            int viError = visa32.viClose(session);
+            session = 0;
+            viError = visa32.viClose(resourceMrg);
+            resourceMrg = 0;
+            return viError;
+        }
+    }
+}
