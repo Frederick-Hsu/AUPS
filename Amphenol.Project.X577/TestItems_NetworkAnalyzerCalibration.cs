0dfb65134403e6d9c014b4355a6470c45296732e
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-12-13 15:41:46 +0800

    Implement some test item functions for the purpose of calibration procedure especially in this specific code file.

---
+++ Amphenol.Project.X577/TestItems_NetworkAnalyzerCalibration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Amphenol.Project.X577
+{
+    partial class TestItems
+    {
+        #region Network analyzer E5071C calibration procedure
+        private static bool SelectNetworkAnalyzerCalibrationKit(List<string> stepParameters,
+                                                                out string stepResult,
+                                                                out string stepStatus,
+                                                                out string stepErrorCode,
+                                                                out string stepErrorDesc)
+        {
+            string chnum = stepParameters[0], calibrationKitNum = stepParameters[1];
+            int successFlag = networkAnalyzer.SelectCalibrationKit(Convert.ToUInt32(chnum), Convert.ToUInt32(calibrationKitNum));
+            if (successFlag == 0)
+            {
+                stepResult = "OK";
+                stepStatus = "Pass";
+                stepErrorCode = string.Empty;
+                stepErrorDesc = string.Empty;
+            }
+            else
+            {
+                stepResult = "NG";
+                stepStatus = "Fail";
+                stepErrorCode = "CKITERR";
+                stepErrorDesc = "Failed to select the calibration kit number";
+            }
+            return (successFlag == 0);
+        }
+        #endregion
+    }
+}
