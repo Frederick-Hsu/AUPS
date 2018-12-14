7ac4e118e56288a0358e7c88f9cdd634a40d4864
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-12-14 11:30:50 +0800

    新增2个测试项函数： QueryNetworkAnalyzerCalibrationKitNumber(...)     PerformOpenCalibration(...)

--- Amphenol.Project.X577/TestItems_NetworkAnalyzerCalibration.cs
+++ Amphenol.Project.X577/TestItems_NetworkAnalyzerCalibration.cs
@@ -30,6 +30,59 @@ namespace Amphenol.Project.X577
             }
             return (successFlag == 0);
         }
+
+        private static bool QueryNetworkAnalyzerCalibrationKitNumber(List<string> stepParameters,
+                                                                     List<string> stepLimits,
+                                                                     out string stepResult,
+                                                                     out string stepStatus,
+                                                                     out string stepErrorCode,
+                                                                     out string stepErrorDesc)
+        {
+            string chnum = stepParameters[0];
+            uint calikitNumLimit = Convert.ToUInt32(stepLimits[0]);
+            uint caliKitNum;
+            int successFlag = networkAnalyzer.QueryCalibrationKitNumber(Convert.ToUInt32(chnum), out caliKitNum);
+            stepResult = Convert.ToString(caliKitNum);
+            if ((successFlag == 0) && (caliKitNum == calikitNumLimit))
+            {
+                stepStatus = "Pass";
+                stepErrorCode = "";
+                stepErrorDesc = "";
+            }
+            else
+            {
+                stepStatus = "Fail";
+                stepErrorCode = "CKITNUM";
+                stepErrorDesc = "Failed to query the calibration kit number.";
+            }
+            return (successFlag == 0);
+        }
+
+        private static bool PerformOpenCalibration(List<string> stepParameters,
+                                                   out string stepResult,
+                                                   out string stepStatus,
+                                                   out string stepErrorCode,
+                                                   out string stepErrorDesc)
+        {
+            string channelNum = stepParameters[0], portNumber = stepParameters[1];
+            int successFlag = networkAnalyzer.SetCalibrationTypeOpen(Convert.ToUInt32(channelNum), Convert.ToUInt32(portNumber));
+            successFlag = networkAnalyzer.MeasureOpenCalibrationData(Convert.ToUInt32(channelNum), Convert.ToUInt32(portNumber));
+            if (successFlag == 0)
+            {
+                stepResult = "OK";
+                stepStatus = "Pass";
+                stepErrorCode = string.Empty;
+                stepErrorDesc = string.Empty;
+            }
+            else
+            {
+                stepResult = "NG";
+                stepStatus = "Fail";
+                stepErrorCode = "OPENCALI";
+                stepErrorDesc = "Failed to perform the open calibration.";
+            }
+            return (successFlag == 0);
+        }
         #endregion
     }
 }
