d0c2f3e25fcb033ca413925af0bb1408097b02ba
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-12-18 11:49:56 +0800

    1. Insert a table to display the calibration kit number vs. kit type relationship.
    2. Implement the following methods of class NetworkAnalyzer_E5071C :
    
    	public int SetCalibrationTriggerSource(uint channelNumber, string on_off = "ON");
    	public int QueryCalibrationTriggerSource(uint channelNumber, out string triggerSource);
    	public int MeasureLoadCalibrationData(uint channelNumber, uint portNumber);
    	public int MeasureThroughCalibrationData(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber);
    	public int MeasureIsolationCalibrationData(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber);
    	public int ExecuteThroughMeasurementForTRLCalibration(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber);
    	public int ExecuteReflectionMeasurementForTRLCalibration(uint channelNumber, uint portNumber);
    	public int ExecuteLineMatchMeasurementForTRLCalibration(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber);
    	public int CalculateCalibrationCoefficientsForResponseType(uint channelNumber);
    	public int CalculateCalibrationCoefficientsForSimplifiedType(uint channelNumber);

--- Amphenol.Instruments/Keysight/NetworkAnalyzer_E5071C_PerformCalibration.cs
+++ Amphenol.Instruments/Keysight/NetworkAnalyzer_E5071C_PerformCalibration.cs
@@ -7,8 +7,57 @@ namespace Amphenol.Instruments.Keysight
     {
         #region Performing calibration
         /* :SENSe1:CORRection:COLLect:CKIT:SELect 1 */
-        public int SelectCalibrationKit(uint chnum, uint calibrationKitNumber = 1)
+        public int SelectCalibrationKitType(uint chnum, uint calibrationKitNumber = 1)
         {
+            /* Select the calibration kit type :
+             * 
+             * Kit number       | Kit type
+             * =================+==================================================
+             *      1           |   85033E
+             * =================+==================================================
+             *      2           |   85033D
+             * =================+==================================================
+             *      3           |   85052D
+             * =================+==================================================
+             *      4           |   85032F
+             * =================+==================================================
+             *      5           |   85032B/E
+             * =================+==================================================
+             *      6           |   85036B/E
+             * =================+==================================================
+             *      7           |   85031B
+             * =================+==================================================
+             *      8           |   85050C/D
+             * =================+==================================================
+             *      9           |   85052C
+             * =================+==================================================
+             *      10          |   85038A/F/M
+             * =================+==================================================
+             *      11          |   85054D
+             * =================+==================================================
+             *      12          |   85056D
+             * =================+==================================================
+             *      13          |   85056K
+             * =================+==================================================
+             *      14          |   85039B
+             * =================+==================================================
+             *      15          |   X11644A
+             * =================+==================================================
+             *      16          |   P11644A
+             * =================+==================================================
+             *      17          |   K11644A
+             * =================+==================================================
+             *      18          |   85050B
+             * =================+==================================================
+             *      19          |   85052B
+             * =================+==================================================
+             *      20          |   85054B
+             * =================+==================================================
+             *      21          |   85056A
+             * =================+==================================================
+             *      22          |   User
+             * =================+==================================================  
+             */
             int error = 0, count = 0;
             string command = ":SENSe" + chnum + ":CORRection:COLLect:CKIT:SELect " + calibrationKitNumber + "\n";
             error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
@@ -72,6 +121,40 @@ namespace Amphenol.Instruments.Keysight
             return QueryErrorStatus(out response);
         }
 
+        /* :SENSe1:CORRection:TRIGger:FREE:STATe ON */
+        public int SetCalibrationTriggerSource(uint channelNumber, string on_off = "ON")
+        {
+            int error = 0, count = 0;
+            string command = ":SENSe" + channelNumber + ":CORRection:TRIGger:FREE:STATe " + on_off.ToUpper() + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            string response;
+            return QueryErrorStatus(out response);
+        }
+
+        /* :SENSe1:CORRection:TRIGger:FREE:STATe? */
+        public int QueryCalibrationTriggerSource(uint channelNumber, out string triggerSource)
+        {
+            int error = 0, count = 0;
+            string command = ":SENSe" + channelNumber + ":CORRection:TRIGger:FREE:STATe?\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            byte[] response = new byte[64];
+            error = visa32.viRead(analyzerSession, response, 64, out count);
+            int stateNumber = Convert.ToInt32(Encoding.ASCII.GetString(response, 0, count));
+            if (stateNumber == 1)
+            {
+                triggerSource = "Internal";
+            }
+            else if (stateNumber == 0)
+            {
+                triggerSource = "System";
+            }
+            else
+            {
+                triggerSource = "Internal";
+            }
+            return error;
+        }
+
         /* :SENSe1:CORRection:COLLect:METHod:TYPE? */
         public int QuerySelectedCalibrationType(uint chnum, out string calibrationType)
         {
@@ -95,6 +178,7 @@ namespace Amphenol.Instruments.Keysight
             error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
             byte[] response = new byte[64];
             error = visa32.viRead(analyzerSession, response, 64, out count);
+            // string retval = Encoding.ASCII.GetString(response, 0, count);
             if (1 == Convert.ToInt32(Encoding.ASCII.GetString(response, 0, count-1)))
             {
                 return 0;
@@ -124,6 +208,160 @@ namespace Amphenol.Instruments.Keysight
             {
                 return (-2);
             }
+        }
+
+        /* :SENSe1:CORRection:COLLect:ACQuire:LOAD 1 */
+        public int MeasureLoadCalibrationData(uint channelNumber, uint portNumber)
+        {
+            int error = 0, count = 0;
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:ACQuire:LOAD " + portNumber + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            command = "*OPC?\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            byte[] response = new byte[64];
+            error = visa32.viRead(analyzerSession, response, 64, out count);
+            if (Convert.ToUInt32(Encoding.ASCII.GetString(response, 0, count-1)) == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return (-3);
+            }
+        }
+
+        /* :SENSe1CORRection:COLLect:ACQuire:THRU 1,4 */
+        public int MeasureThroughCalibrationData(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber)
+        {
+            int error = 0, count = 0;
+            if (responsePortNumber == stimulusPortNumber)
+            {
+                return (-1);
+            }
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:ACQuire:THRU " + responsePortNumber + "," + stimulusPortNumber + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            command = "*OPC?\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            byte[] response = new byte[32];
+            error = visa32.viRead(analyzerSession, response, 32, out count);
+            if (Convert.ToUInt32(Encoding.ASCII.GetString(response, 0, count-1)) == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return (-4);
+            }
+        }
+
+        /* :SENSe1:CORRection:COLLect:ACQuire:ISOLation 1,2 */
+        public int MeasureIsolationCalibrationData(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber)
+        {
+            int error = 0, count = 0;
+            if (responsePortNumber == stimulusPortNumber)
+            {
+                return (-1);
+            }
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:ACQuire:ISOLation " + responsePortNumber + "," + stimulusPortNumber + ";\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            command = "*OPC?\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            byte[] response = new byte[32];
+            error = visa32.viRead(analyzerSession, response, 32, out count);
+            if (Convert.ToUInt32(Encoding.ASCII.GetString(response, 0, count-1)) == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return (-5);
+            }
+        }
+
+        /* :SENSe1:CORRection:COLLect:ACQuire:TRLThru 2,3 */
+        public int ExecuteThroughMeasurementForTRLCalibration(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber)
+        {
+            int error = 0, count = 0;
+            if (responsePortNumber == stimulusPortNumber)
+            {
+                return (-1);
+            }
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:ACQuire:TRLThru " + responsePortNumber + ", " + stimulusPortNumber + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            command = "*OPC?\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            byte[] response = new byte[64];
+            error = visa32.viRead(analyzerSession, response, 64, out count);
+            if (Convert.ToUInt32(Encoding.ASCII.GetString(response, 0, count-1)) == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return (-6);
+            }
+        }
+
+        /* :SENSe1:CORRection:COLLect:ACQuire:TRLReflect 1 */
+        public int ExecuteReflectionMeasurementForTRLCalibration(uint channelNumber, uint portNumber)
+        {
+            int error = 0, count = 0;
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:ACQuire:TRLReflect " + portNumber + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            command = "*OPC?\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            byte[] response = new byte[64];
+            error = visa32.viRead(analyzerSession, response, 64, out count);
+            if (Convert.ToUInt32(Encoding.ASCII.GetString(response, 0, count-1)) == 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return (-7);
+            }
+        }
+
+        /* :SENSe1:CORRection:COLLect:ACQuire:TRLLine 1, 2 */
+        public int ExecuteLineMatchMeasurementForTRLCalibration(uint channelNumber, uint responsePortNumber, uint stimulusPortNumber)
+        {
+            int error = 0, count = 0;
+            if (responsePortNumber == stimulusPortNumber)
+            {
+                return (-1);
+            }
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:ACQuire:TRLLine " + responsePortNumber + ", " + stimulusPortNumber + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            command = "*OPC?\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            byte[] response = new byte[64];
+            error = visa32.viRead(analyzerSession, response, 64, out count);
+            if (Convert.ToUInt32(Encoding.ASCII.GetString(response, 0, count-1)) == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (-8);
+            }
+        }
+
+        /* :SENSe1:CORRection:COLLect:SAVE */
+        public int CalculateCalibrationCoefficientsForResponseType(uint channelNumber)
+        {
+            int error = 0, count = 0;
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:SAVE\n", response;
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
+        }
+
+        /* :SENSe1:CORRection:COLLect:SIMPlified:SAVE */
+        public int CalculateCalibrationCoefficientsForSimplifiedType(uint channelNumber)
+        {
+            int error = 0, count = 0;
+            string command = ":SENSe" + channelNumber + ":CORRection:COLLect:SIMPlified:SAVE\n", response;
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
         }
         #endregion
     }
