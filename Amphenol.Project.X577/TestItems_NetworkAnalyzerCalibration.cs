8a4ad7430a5d6979c3272e96bd849a6d31884aae
Frederick Hsu <frederick.xu@amphenolmcp.com>
2018-12-18 13:32:54 +0800

    Insert a swith(...) {case ...} branch-selection code block to convert the calibration kit type to kit number.

--- Amphenol.Project.X577/TestItems_NetworkAnalyzerCalibration.cs
+++ Amphenol.Project.X577/TestItems_NetworkAnalyzerCalibration.cs
@@ -12,8 +12,35 @@ namespace Amphenol.Project.X577
                                                                 out string stepErrorCode,
                                                                 out string stepErrorDesc)
         {
-            string chnum = stepParameters[0], calibrationKitNum = stepParameters[1];
-            int successFlag = networkAnalyzer.SelectCalibrationKit(Convert.ToUInt32(chnum), Convert.ToUInt32(calibrationKitNum));
+            string chnum = stepParameters[0], calibrationKitType = stepParameters[1];
+            uint calibrationKitNum;
+            switch (calibrationKitType.ToUpper())
+            {
+                case "85033E":      calibrationKitNum =  1;     break;
+                case "85033D":      calibrationKitNum =  2;     break;
+                case "85052D":      calibrationKitNum =  3;     break;
+                case "85032F":      calibrationKitNum =  4;     break;
+                case "85032B/E":    calibrationKitNum =  5;     break;
+                case "85036B/E":    calibrationKitNum =  6;     break;
+                case "85031B":      calibrationKitNum =  7;     break;
+                case "85050C/D":    calibrationKitNum =  8;     break;
+                case "85052C":      calibrationKitNum =  9;     break;
+                case "85038A/F/M":  calibrationKitNum = 10;     break;
+                case "85054D":      calibrationKitNum = 11;     break;
+                case "85056D":      calibrationKitNum = 12;     break;
+                case "85056K":      calibrationKitNum = 13;     break;
+                case "85039B":      calibrationKitNum = 14;     break;
+                case "X11644A":     calibrationKitNum = 15;     break;
+                case "P11644A":     calibrationKitNum = 16;     break;
+                case "K11644A":     calibrationKitNum = 17;     break;
+                case "85050B":      calibrationKitNum = 18;     break;
+                case "85052B":      calibrationKitNum = 19;     break;
+                case "85054B":      calibrationKitNum = 20;     break;
+                case "85056A":      calibrationKitNum = 21;     break;
+                case "USER":        calibrationKitNum = 22;     break;
+                default:            calibrationKitNum =  1;     break;
+            }
+            int successFlag = networkAnalyzer.SelectCalibrationKitType(Convert.ToUInt32(chnum), calibrationKitNum);
             if (successFlag == 0)
             {
                 stepResult = "OK";
