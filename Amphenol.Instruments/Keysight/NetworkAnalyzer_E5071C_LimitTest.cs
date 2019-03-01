3864dfffc7ade2b22a0134c1779dd17974a2cde8
Frederique Hsu <frederique.hsu@outlook.com>
2019-03-01 14:05:37 +0800

    Add a new code file NetworkAnalyzer_E5071C_LimitTest.cs into the Amphenol.Instruments.csproj, to implement the section of limit test.

---
+++ Amphenol.Instruments/Keysight/NetworkAnalyzer_E5071C_LimitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Amphenol.Instruments.Keysight
+{
+    partial class NetworkAnalyzer_E5071C
+    {
+        #region Performing a Limit Test
+        /* :CALCulate1:SELected:LIMit:DISPlay:STATe ON */
+        public int TurnOnOffLimitLineDisplay(uint channelNum, string onOff = "OFF")
+        {
+            int error = 0, count = 0;
+            string command = ":CALCulate" + channelNum + ":SELected:LIMit:DISPlay:STATe " + onOff + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            string response;
+            return QueryErrorStatus(out response);
+        }
+
+        /* :CALCulate1:SELected:LIMit:STATe ON */
+        public int TurnOnOffLimitTestFunction(uint channelNum, string onOff = "OFF")
+        {
+            int error = 0, count = 0;
+            string command = ":CALCulate" + channelNum + ":SELected:LIMit:STATe " + onOff + "\n";
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            string response;
+            return QueryErrorStatus(out response);
+        }
+        #endregion
+    }
+}
