454cde0a8e7a4b7f00d1fed082b0a6842afe8d63
Frederique Hsu (徐赞) <frederique.hsu@outlokk.com>
2019-03-06 20:28:08 +0800

    1. Insert a detailed comment to describe the meaning of argument of function SelectInstrumentStateContentTypeToSave(...)
    2. Implement a new function public int SelectWhichSettingsOfChannelsTracesToSave(string onOff = "OFF");

--- Amphenol.Instruments/Keysight/NetworkAnalyzer_E5071C_SavingRecalling.cs
+++ Amphenol.Instruments/Keysight/NetworkAnalyzer_E5071C_SavingRecalling.cs
@@ -10,6 +10,12 @@ namespace Amphenol.Instruments.Keysight
         /* :MMEMory:STORe:STYPe CDSTate */
         public int SelectInstrumentStateContentTypeToSave(string instrumentStateContentType = "CSTate")
         {
+            /* [NOTICE] : For the instrument state content type, only 4 options are available.
+             *            STATe     - Instrument Status only
+             *            CSTate    - Instrument status and calibration coefficient array
+             *            DSTate    - Instrument status, corrected data / memory array (measurement data)
+             *            CDSTate   - Instrument status, calibration coefficient array, and corrected data / memory array (measurement data)
+             */
             int error = 0, count = 0;
             string command = ":MMEMory:STORe:STYPe " + instrumentStateContentType + "\n";
             error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
@@ -27,6 +33,22 @@ namespace Amphenol.Instruments.Keysight
             error = visa32.viRead(analyzerSession, response, 64, out count);
             instrumentStateContentType = Encoding.ASCII.GetString(response, 0, count - 1);
             return error;
+        }
+
+        /* :MMEMory:STORe:SALL OFF */
+        public int SelectWhichSettingsOfChannelsTracesToSave(string onOff = "OFF")
+        {
+            /* [NOTICE] : select whether to save the settings of all channels / traces or that of the 
+             *            displayed channels / traces only as the instrument state to be saved.
+             *            
+             *            ON    - specify the settings of all channels / traces as the target to be saved.
+             *            OFF   - specify the settings of displayed channels / traces only as the target to be saved.
+             *            
+             */
+            int error = 0, count = 0;
+            string command = ":MMEMory:STORe:SALL " + onOff + "\n", response;
+            error = visa32.viWrite(analyzerSession, Encoding.ASCII.GetBytes(command), command.Length, out count);
+            return QueryErrorStatus(out response);
         }
         #endregion
     }
