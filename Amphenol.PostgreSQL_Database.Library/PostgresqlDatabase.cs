337b2d0d1ab327a9a600b903bbfec19a07d61642
Frederique Hsu (徐赞) <frederique.hsu@outlook.com>
2019-04-22 17:21:55 +0800

    Define and implement the class PostgresqlDatabase, this class will allow developer easily access the PostgreSQL database server in C# programming.
    meanwhile it provides a comprehensive suite of utilities methods to interact with psql over native SQL statement.

---
+++ Amphenol.PostgreSQL_Database.Library/PostgresqlDatabase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Npgsql;
+
+namespace Amphenol.PostgreSQL_Database.Library
+{
+    public partial class PostgresqlDatabase
+    {
+        private NpgsqlConnection psqlConnection;
+        private string connectionStr;
+
+        public PostgresqlDatabase(string dbServerIP, string dbName)
+        {
+            connectionStr = string.Format("Server = {0}; Database = {1}; User Id = {2}; Password = {3}; Port = {4};",
+                                          dbServerIP,
+                                          dbName,
+                                          "amphenol_production_test",
+                                          "Apt520*!",
+                                          "5432");
+        }
+
+        public ConnectionState ConnectAndOpenDatabase()
+        {
+            try
+            {
+                psqlConnection = new NpgsqlConnection(connectionStr);
+            }
+            catch (NpgsqlException exception)
+            {
+                Console.WriteLine("Exception message: {0}", exception.Message);
+                throw;
+            }
+            return psqlConnection.State;
+        }
+
+        public ConnectionState CloseDatabase()
+        {
+            // psqlConnection.Dispose();
+            psqlConnection.Close();
+            return psqlConnection.State;
+        }
+
+        public int ExecuteQuerySql(string selectSqlCmd, out DataTable queriedTable)
+        {
+            NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(selectSqlCmd, psqlConnection);
+            DataSet dtset = new DataSet();
+            dtset.Reset();
+            dataAdapter.Fill(dtset);
+            queriedTable = new DataTable();
+            queriedTable = dtset.Tables[0];
+            return 0;
+        }
+
+        public int ExecuteInsertSql(string insertSqlCmd)
+        {
+            NpgsqlCommand command = new NpgsqlCommand(insertSqlCmd, psqlConnection);
+            int affectedRowsNumber = command.ExecuteNonQuery();
+            return affectedRowsNumber;
+        }
+
+        public int ExecuteUpdateSql(string updateSqlCmd)
+        {
+            NpgsqlCommand command = new NpgsqlCommand(updateSqlCmd, psqlConnection);
+            int affectedRowsNumber = command.ExecuteNonQuery();
+            return affectedRowsNumber;
+        }
+
+        public int ExecuteDeleteSql(string deleteSqlCmd)
+        {
+            NpgsqlCommand command = new NpgsqlCommand(deleteSqlCmd, psqlConnection);
+            int affectedRowsNumber = command.ExecuteNonQuery();
+            return affectedRowsNumber;
+        }
+    }
+}
