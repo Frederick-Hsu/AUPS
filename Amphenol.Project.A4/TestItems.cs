6047049c1d0095e56c53c63d5d547852c634fbdb
Frederick Hsu <frederick.xu@amphenolmcp.com>
2019-06-25 11:33:48 +0800

    1. Organize 6 more case branches of test functions in the Amphenol.Project.A4.Execute(...) function
    2. Accomplish to write the hints of test function and its corresponding parameter list for 6 newly-added test functions.

--- Amphenol.Project.A4/TestItems.cs
+++ Amphenol.Project.A4/TestItems.cs
@@ -27,6 +27,24 @@ namespace Amphenol.Project.A4
                 case "CloseNetworkAnalyzerE5071C":
                     success = CloseNetworkAnalyzerE5071C(out result, out status, out errorCode, out errorDesc);
                     break;
+                case "PresetNetworkAnalyzerWindowLayoutDisplay":
+                    success = PresetNetworkAnalyzerWindowLayoutDisplay(out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "ConfigWindowDisplayLayout":
+                    success = ConfigWindowDisplayLayout(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "AllocateTraceAmountForChannel":
+                    success = AllocateTraceAmountForChannel(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "ConfigTraceDisplayModeInChannel":
+                    success = ConfigTraceDisplayModeInChannel(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "ConfigureMeasurementSParameterAndFormat":
+                    success = ConfigureMeasurementSParameterAndFormat(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "ConfigSweepFreqRange":
+                    success = ConfigSweepFreqRange(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
             }
             stepResult = result;
             stepStatus = status;
@@ -49,6 +67,49 @@ namespace Amphenol.Project.A4
             testFnctInfo.Add("CloseNetworkAnalyzerE5071C", new List<string>(new string[]
             {
                 "Close the connection between host PC and network analyzer Keysight E5071C."
+            }));
+            testFnctInfo.Add("PresetNetworkAnalyzerWindowLayoutDisplay", new List<string>(new string[]
+            {
+                "Preset the window layout and display for network analyzer Keysight E5071C."
+            }));
+            testFnctInfo.Add("ConfigWindowDisplayLayout", new List<string>(new string[] 
+            {
+                "Select the window layout (i.e. channel display mode) for network analyzer front panel",
+                "Window layout codename, such as D1, D12, D1_2, D12_34, ..., etc. \n" +
+                "For more details of window layout, please refer to the programming guide of Keysight E5071C."
+            }));
+            testFnctInfo.Add("AllocateTraceAmountForChannel", new List<string>(new string[]
+            {
+                "Allocate how many traces you want to accommondate in the specific channel",
+                "The channel number you want to specify, range : 1 ~ 9",
+                "The amount of traces, range : 1 ~ 9"
+            }));
+            testFnctInfo.Add("ConfigTraceDisplayModeInChannel", new List<string>(new string[]
+            {
+                "Configure the trace display mode (i.e. the trace graph layout) in the channel you specified",
+                "Channel number you specified, range : 1 ~ 9",
+                "Trace display mode, such as D1, D12, D1_2, D12_34, ..., etc., \n" +
+                "for more details, please refer to the programming guide of Keysight E5071C."
+            }));
+            testFnctInfo.Add("ConfigureMeasurementSParameterAndFormat", new List<string>(new string[]
+            {
+                "Configure and select the measurement S-Parameter on the trace you specified, \n" +
+                "select the data format for trace, and show the data trace.",
+                "Channel number you selected, range : 1 ~ 9",
+                "Trace number in the channel you specified, range : 1 ~ 9",
+                "S-Parameter for the trace, such as S11, S12, S22, S33, S34, ..., etc.",
+                "Trace data format, \n" +
+                "such as MLOGarithmic, PHASe, GDELay, SLINear, SLOG, SCOMplex, SMITh, SADMittance, PLINear, \n" +
+                "PLOGarithmic, POLar, MLINear, SWR, REAL, IMAGinary, UPHase, PPHase."
+            }));
+            testFnctInfo.Add("ConfigSweepFreqRange", new List<string>(new string[]
+            {
+                "Configure the sweep frequency range, from start frequency to stop frequency",
+                "Channel number, range : 1 ~ 9",
+                "Sweep start frequency (unit : Hertz), you can directly enter the " +
+                "frequency value e.g. 500E6 as scientific notation",
+                "Sweep stop frequency (unit : Hertz), directly enter the frequency value" +
+                "e.g. 3.15E9 as scientific notation"
             }));
             return testFnctInfo;
         }
