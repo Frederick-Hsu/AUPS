038b4c78320c322523848e712cf21d30a3fb7ad7
Frederick Hsu <frederick.xu@amphenolmcp.com>
2019-07-11 15:03:05 +0800

    Append 4 branches for the test functions list,
    and fill out their description tool tips.

--- Amphenol.Project.A4/TestItems.cs
+++ Amphenol.Project.A4/TestItems.cs
@@ -54,6 +54,18 @@ namespace Amphenol.Project.A4
                 case "MaximizeOnOffChannelTraceDisplay":
                     success = MaximizeOnOffChannelTraceDisplay(stepParameters, out result, out status, out errorCode, out errorDesc);
                     break;
+                case "ShowHideRegularMarker":
+                    success = ShowHideRegularMarker(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "TargetMarkerSearch":
+                    success = TargetMarkerSearch(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "TurnOnOffMarkerSearchTrackingFeature":
+                    success = TurnOnOffMarkerSearchTrackingFeature(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
+                case "PerformMarkerSearch":
+                    success = PerformMarkerSearch(stepParameters, out result, out status, out errorCode, out errorDesc);
+                    break;
             }
             stepResult = result;
             stepStatus = status;
@@ -128,14 +140,18 @@ namespace Amphenol.Project.A4
             }));
             testFnctInfo.Add("ConfigureChannelTraceDisplayScale", new List<string>(new string[] 
             {
+                /* test function name description */
                 "Configure the display scale of the user-specified trace inside one certain channel",
+                /* parameter 1 description */
                 "Channel number, range : 1 ~ 9",
+                /* parameter 2 description */
                 "Trace number inside the channel you specify, range : 1 ~ 9",
                 "Number of divisions for the graticule, it is a channel-wide setting, \n" +
                 "shared among all traces inside one certain channel, \n" +
                 "Range : 4 to 30 \n" +
                 "Preset value : 10 \n" +
                 "Resolution : 2",
+                /* parameter 3 description */
                 "The scale per division, when you use the rectangular display format, or \n" +
                 "the full scale value (i.e. the value of outermost circle), when Smith chart / Polar formats \n" +
                 "Range : 1e-18 to 1e8\n" +
@@ -151,10 +167,12 @@ namespace Amphenol.Project.A4
                 "       Phase, Expand Phase, Positive Phase : degree\n" +
                 "       Group Delay : second\n" +
                 "       Others : No unit",
+                /* parameter 4 description */
                 "Number of graticule line\n" +
                 "Range : 0 to number of divisions\n" +
                 "Preset value : 5 (when the data format is \"Linear Magnitude\" or \"SWR\", the preset value is 1.)\n" +
                 "Resolution : 1",
+                /* parameter 5 description */
                 "Value of reference graticule line\n" +
                 "Range : -5e8 to 5e8\n" +
                 "Preset value : 0 (When the data format is \"SWR\", the preset value is 1.)\n" +
@@ -171,6 +189,62 @@ namespace Amphenol.Project.A4
                 "Channel max display ON/OFF, range : \"ON\" or \"OFF\"",
                 "Trace number you want to choose, range : 1 ~ 9",
                 "Trace max ON/OFF display, range : \"ON\" or \"OFF\""
+            }));
+            testFnctInfo.Add("ShowHideRegularMarker", new List<string>(new string[]
+            {
+                "Turn ON/OFF the regular marker display",
+                "Channel number, range : 1 ~ 9",
+                "Trace number, range : 1 ~ 9",
+                "Marker number, range : 1 ~ 9",
+                "Marker display status, ON or OFF"
+            }));
+            testFnctInfo.Add("TargetMarkerSearch", new List<string>(new string[]
+            {
+                /* test function name description */
+                "Define the marker target search by specifying the target value (response value) \n" +
+                "and transitional direction (positive or negative value change).",
+                /* parameter 1 description */
+                "Channel number, range : 1 ~ 9",
+                /* parameter 2 description */
+                "Trace number, range : 1 ~ 9",
+                /* parameter 3 description */
+                "Regular marker number, range : 1 ~ 9",
+                /* parameter 4 description */
+                "Marker search type, by default TARGet, \n" +
+                "MINimum\n" +
+                "MAXimum\n" +
+                "PEAK\n" +
+                "LPEak\n" +
+                "RPEak\n" +
+                "TARGet\n" +
+                "LTARget\n" +
+                "RTARget",
+                /* parameter 5 description */
+                "Target response value for target search, " +
+                "range : -5e8 to 5e8\n" +
+                "unit : varies depending on the data format as follows:\n" +
+                "       Logarithmic Magnitude : dB\n" +
+                "       Phase, Expand Phase, Positive Phase : degree\n" +
+                "       Group Delay : second\n" +
+                "       Others : no unit",
+                /* parameter 6 description */
+                "Target search transitional direction, range : POSitive, NEGative, BOTH by default."
+            }));
+            testFnctInfo.Add("TurnOnOffMarkerSearchTrackingFeature", new List<string>(new string[] 
+            {
+                "Turn ON or OFF the marker search tracking feature for the active trace of channel, \n" +
+                "which performs the marker search every time the trace is updated.",
+                "Channel number, range : 1 ~ 9",
+                "Trace number, range : 1 ~ 9",
+                "Marker number, range : 1 ~ 9",
+                "ON or OFF"
+            }));
+            testFnctInfo.Add("PerformMarkerSearch", new List<string>(new string[]
+            {
+                "Perform the marker search",
+                "Channel number, range : 1 ~ 9",
+                "Trace number, range : 1 ~ 9",
+                "Marker number, range : 1 ~ 9"
             }));
             return testFnctInfo;
         }
